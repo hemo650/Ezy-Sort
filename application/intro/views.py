482d522454192f9e8dff8b8d979452376cd0f047
Abdi <amohamud23@gmail.com>
2019-10-03 13:28:34 -0700

    added the api to the master code

--- application/intro/views.py
+++ application/intro/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts \
     import HttpResponse, render
+import requests
+import json
+
+
 
 
 def index(request):
@@ -30,6 +34,7 @@ def Note1(request):
     return render(request, 'intro/Note1.html')
 def Note2(request):
     return render(request, 'intro/Note2.html')
+
 def Note3(request):
     return render(request, 'intro/Note3.html')
 
@@ -37,4 +42,25 @@ def Login(request):
     return render(request, 'intro/Login.html')
 
 def addItem(request):
-    return render(request, 'intro/addPage.html')
+    if request.method == 'POST' and request.FILES['myfile']:
+        URL_post = 'https://api.tabscanner.com/KuxAxBfl8w4FvTaNwqwHD3ajxzQBOoyVuaYqRXcgUPKQbtPezCMmxBloThkV3Ico/process'
+        myImage = request.FILES['myfile']
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        # imageArray = np.array(myImage)
+        files = {'receiptImage': myImage}
+        r = requests.post(url=URL_post, files=files)
+        json_data = json.loads(r.text)
+        token = json_data['token']
+        URL_get = 'https://api.tabscanner.com/KuxAxBfl8w4FvTaNwqwHD3ajxzQBOoyVuaYqRXcgUPKQbtPezCMmxBloThkV3Ico/result/' + token
+        j = requests.get(url=URL_get)
+        result_json = json.loads(j.text)
+        while(result_json['status'] == "pending"):
+            print("pending")
+            j = requests.get(url=URL_get)
+            result_json = json.loads(j.text)
+        print(j.text)
+
+    if request.method == 'GET':
+        result_json = {}
+
+    return render(request, 'intro/addPage.html', {'list': result_json})
