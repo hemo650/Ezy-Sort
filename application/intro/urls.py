325bddb7ceb17bdb60f5ed82824e2d8a08a03e79
Carolyn <carolyn.chan.01@gmail.com>
2019-12-18 20:22:09 -0800

    Revert "Revert "Merge branch 'development' of https://github.com/CSC-648-SFSU/csc648-03-fa19-Team102 into development""
    This reverts commit 6299a86212d418318703e7836b3dffccd587aeeb.

--- application/intro/urls.py
+++ application/intro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page, name='index'),
+    path('', views.register, name='index'),
     path('anne/', views.anne, name='anne'),
     path('abdi/', views.abdi, name='abdi'),
     path('carolyn/', views.carolyn, name='carolyn'),
@@ -17,7 +17,10 @@ urlpatterns = [
     path('addItem/', views.addItem, name='addItem'),
     path('profile/', views.profile_page, name='profile'),
     path('home/', views.home_page, name='home'),
-    path('refrigerator/', views.addItem, name='refrigerator'),
+    path('addItem/', views.addItem, name='addItem'),
     path('shoppingList/', views.shoppingList, name='shoppingList'),
+    path('refrigerator/upload', views.searchbar, name='searchbar'),
+    path('refrigerator/search/', views.showItems, name='showItems'),
+    path('refridgerator/', views.inventory, name='refrigerator'),
 
 ]
